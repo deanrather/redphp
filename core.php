fbc54206f16cb96eeafa77b288d0d2d21e4d2d5d
Dean Rather <deanrather@gmail.com>
2012-04-27 21:34:17 +1000

    now allows more dynamic controllers

--- core.php
+++ core.php
@@ -61,17 +61,16 @@
 				/**		 * Loads your projects' app.php but does not yet call init();		 */
 		private function loadApp()
 		{			if(file_exists('../app/app.php'))			{				require_once('../app/app.php');				$this->app = new app($this);			}			if(file_exists('../app/view.php'))			{				require_once('../app/view.php');			}			if(file_exists('../app/controller.php'))			{				require_once('../app/controller.php');			}
-		}				/**		 * Loads the controller, tells it which page it will load later.		 * The layout will tell the controller to load the page.		 */		private function loadController()		{
-			$app = (isset($this->uri[0]) ? $this->uri[0] : 'index');
-			$controller = '../app/'.$app.'/'.$app.'Controller.php';			if(!file_exists($controller)) {
-				$oldController = $controller;
-				$controller = '../app/index/defaultController.php';
-				$app = 'default';
-				if(!file_exists($controller)) {					$newInstallMsg="<hr />If you're still setting up redphp, perhaps you haven't configuerd etc/config.ini's 'index_dir'.";
-					$this->error("You need [ <b>$oldController</b> ] or [<b>$controller</b>].$newInstallMsg");
-				}
+		}				/**		 * Loads the controller, tells it which page it will load later.		 * The layout will tell the controller to load the page.		 */		private function loadController()		{			$app = (isset($this->uri[0]) ? $this->uri[0] : 'index');			$page = (isset($this->uri[1]) ? $this->uri[1] : 'index');
+			$controller = '../app/'.$app.'/'.$page.'Controller.php';			if(!file_exists($controller)) {				$controller = "../app/$app/{$app}Controller.php";				$page = $app;				if(!file_exists($controller)) {
+					$oldController = $controller;
+					$controller = '../app/index/defaultController.php';
+					$page = 'default';
+					if(!file_exists($controller)) {						$newInstallMsg="<hr />If you're still setting up redphp, perhaps you haven't configuerd etc/config.ini's 'index_dir'.";
+						$this->error("You need [ <b>$oldController</b> ] or [<b>$controller</b>].$newInstallMsg");
+					}				}
 			}
-			require_once($controller);			$controller = $app.'Controller';			if(!class_exists($controller)) $this->error("You need [ <b>class $controller extends controller</b> ].");			$this->controller = new $controller($this);
+			require_once($controller);			$controller = $page.'Controller';			if(!class_exists($controller)) $this->error("You need [ <b>class $controller extends controller</b> ].");			$this->controller = new $controller($this);
 		}				/**		 * Loads the layout.		 * The layout will tell the controller to load its page.		 */		private function display()		{			require_once('../app/_layouts/'.$this->pageDetails['layout'].'Layout.php');		}				private function googleAnalytics($key)		{			$return = '';						if($key)			{				$return = <<<GOOGLEANALYTICS<!-- Google Analytics Code --><script type="text/javascript"> var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));</script> <script type="text/javascript"> try {var pageTracker = _gat._getTracker("$key");pageTracker._trackPageview();} catch(err) {}</script><!-- / Google Analytics Code -->GOOGLEANALYTICS;			}						return $return;		}				/**		 * If 'debug' is defined in the config, it will print an error message,		 * Otherwise, it just prints a '404' for the user.		 */		public function error($message = '')		{
 			$log = '';
 			$log.= "<!--\n";
